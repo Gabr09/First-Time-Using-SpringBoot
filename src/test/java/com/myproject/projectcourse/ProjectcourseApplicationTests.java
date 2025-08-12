6b9bf51b05668da46ed2b049988ed466b4c6f0e9
Gabriel D <igabrielpereira09@gmail.com>
2025-08-12 10:43:33 -0300

    Project created!

---
+++ src/test/java/com/myproject/projectcourse/ProjectcourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.projectcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectcourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
